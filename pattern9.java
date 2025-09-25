50530c82d86ca021b470e1d1d3e2318b7dc1f39e
Mishika Bhandari <mishikabhandaris@gmail.com>
2025-09-25 09:04:35 +0530

    Pattern 9 loops

---
+++ pattern9.java
@@ -0,0 +1,22 @@
+// Pattern 9
+// 1. Write a program to print the following pattern:
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+public class pattern9 {
+    public static void main(String [] args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                 System.out.print("1 ");
+                }
+                else{
+                    System.out.print("0 ");
+                }    
+        }
+        System.out.println();
+    }
+}
+}
