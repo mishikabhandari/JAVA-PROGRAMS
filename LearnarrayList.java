49c630dcc6774ac829d155966450e1538d7fc326
Mishika Bhandari <mishikabhandaris@gmail.com>
2025-10-24 13:37:32 +0530

    learned how to implement arraylist in java

---
+++ LearnarrayList.java
@@ -0,0 +1,13 @@
+// Create an ArrayList of integers and write a method to add the numbers 1 to 5 into it. Then print the list.
+// Bonus: What is the time complexity of adding an element to an ArrayList?
+
+import java.util.ArrayList;
+public class LearnarrayList{
+    public static void main(String [] args){
+        ArrayList <Integer> numbers = new ArrayList <Integer> ();
+        for(int i=1;i<=5;i++){
+            numbers.add(i);
+        }
+        System.out.println("The given array list is : " + numbers);
+    }
+}
